6f9af37b0cd47e0ab8ce840c076901d06538b387
Tel36071486 <diego.mtz.ra@gmail.com>
2022-04-23 22:29:40 -0500

    Primer commit "verificador de Fecha"

--- calendario.cpp
+++ calendario.cpp
@@ -1,19 +1,34 @@
-// calendario.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int today, month, year;
+
+	std::cout << "Acontinuacion ingrese la fecha del dia de hoy. " << std::endl;
+	std::cout << "Ingrese el dia: ";
+	std::cin >> today;
+	std::cout << "Ingrese el mes: ";
+	std::cin >> month;
+	std::cout << "Ingrese el año: ";
+	std::cin >> year;
+
+	if (today <= 31 && today >=01) {
+		if (month <= 12 && month >= 01) {
+			if (year >= 2000) {
+
+			}
+			else {
+				std::cout << "Error en la fecha. ";
+			}
+		}
+		else {
+			std::cout << "Error en la fecha. ";
+		}
+	}
+	else {
+		std::cout << "Error en la fecha.";
+	}
+
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
